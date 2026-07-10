9aac84db26a20621e067c5e9bd122c4b92715558
pindhe <kharash420@gmail.com>
2026-07-10 16:38:52 +0300

    .

--- admin/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once __DIR__ . '/includes/bootstrap.php';
-
-if (is_admin_logged_in()) {
-    log_activity('admin_logout', 'Admin logged out');
-}
-admin_logout();
-flash('info', 'You have been signed out.');
-redirect('admin/login.php');
